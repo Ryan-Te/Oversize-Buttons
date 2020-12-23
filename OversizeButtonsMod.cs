c8bba152405c994db2fd3c778febba05a4ac5975
Ryan T <46229874+Ryan-Te@users.noreply.github.com>
2020-12-22 23:26:14 -0500

    Update OversizeButtonsMod.cs
    Added support for PiTUNG 2.4.X and made it easier to add more button sizes

--- OversizeButtonsMod.cs
+++ OversizeButtonsMod.cs
@@ -1,28 +1,61 @@
-﻿using PiTung;
+using PiTung;
 using PiTung.Components;
+using PiTung.Console;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 
 public class OversizeButtons : Mod
 {
-    public override string Name => "Oversize Buttons";
-    public override string PackageName => "me.jimmy.GiantButtons";
-    public override string Author => "Iamsodarncool";
-    public override Version ModVersion => new Version("1.0");
+    public override string Name => "Oversize Buttons For PiTUNG 2.4.X";
+    public override string PackageName => "me.jimmy.GiantButtons.PiTUNG2.4.X";
+    public override string Author => "Iamsodarncool, Modified by Ryan";
+    public override Version ModVersion => new Version("1.1");
 
 
     public override void BeforePatch()
     {
-        ComponentRegistry.CreateNew(this, "LargePanelButton", "Large Panel Button", CreatePanelButtonOfSize(3, 2));
-        ComponentRegistry.CreateNew(this, "GiantPanelButton", "Giant Panel Button", CreatePanelButtonOfSize(3, 3));
-        ComponentRegistry.CreateNew(this, "EnormousPanelButton", "Enormous Panel Button", CreatePanelButtonOfSize(15, 2));
-        
-        // the world is not ready
-        // ComponentRegistry.CreateNew("CollosalPanelButton", "Collosal Panel Button", CreatePanelButtonOfSize(51, 51));
+        ComponentRegistry.CreateNew("LargePanelButton", "Large Panel Button", CreatePanelButtonOfSize(3, 2));
+        ComponentRegistry.CreateNew("GiantPanelButton", "Giant Panel Button", CreatePanelButtonOfSize(3, 3));
+        ComponentRegistry.CreateNew("EnormousPanelButton", "Enormous Panel Button", CreatePanelButtonOfSize(15, 2));
+
+        // This game's sequal has been in devolpment for over 2 years. I hope the world is ready
+        ComponentRegistry.CreateNew("CollosalPanelButton", "Collosal Panel Button", CreatePanelButtonOfSize(51, 51));
+        // On second thought, the world is still not ready to see this one
         // ComponentRegistry.CreateNew("TitanicPanelButton", "Titanic Panel Button", CreatePanelButtonOfSize(401, 601));
+
+        // add buttons from file
+        if (File.Exists($"{Directory.GetCurrentDirectory()}/sizes_b.txt")) 
+        {
+            string[] sizes = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/sizes_b.txt");
+            foreach(string size in sizes)
+            {
+                try
+                {
+                    int[] xysize = Array.ConvertAll(size.Split(' '), s => int.Parse(s)); // split the string into a string array at every space and convert that into an int array
+                    ComponentRegistry.CreateNew($"{xysize[0]} x {xysize[1]} PanelButton", $"{xysize[0]} x {xysize[1]} PanelButton", CreatePanelButtonOfSize(xysize[0], xysize[1]));
+                }
+                catch (Exception ex) // Catches all exceptions
+				{
+                    if (ex is ArgumentException)
+                    {
+                        IGConsole.Log($"Error! Cant load button of size {size} twice");
+                    }
+                    else
+                    {
+                        IGConsole.Log($"Error! Cant load button of size {size}, correct format is \"{{X size of button}} {{Y size of button}}\" ");
+                    }
+                }
+            }
+        }
+        Shell.RegisterCommand<Add_Button>();
+        Shell.RegisterCommand<Remove_Button>();
     }
 
-    private static CustomBuilder CreatePanelButtonOfSize(int x, int z)
+    public static CustomBuilder CreatePanelButtonOfSize(int x, int z)
     {
         return PrefabBuilder
             .Custom(() =>
@@ -52,5 +85,100 @@ public class OversizeButtons : Mod
 
                 return obj;
             });
+    }
+}
+public class Add_Button : Command
+{
+    public override string Name => "addButton";
+    public override string Usage => $"{Name} x_size y_size";
+    public override string Description => "Adds a button of specified size to the component list";
+
+    public override bool Execute(IEnumerable<string> args)
+    {
+        if (args.Count() < 2)
+        {
+            IGConsole.Log("Not enough arguments!");
+            return false;
+        }
+        string size = string.Join(" ", args.ToArray()); //turns the array into a string
+        try
+        {
+            int[] xysize = Array.ConvertAll(size.Split(' '), s => int.Parse(s)); // split the string into a string array at every space and convert that into an int array
+            ComponentRegistry.CreateNew($"{xysize[0]} x {xysize[1]} PanelButton", $"{xysize[0]} x {xysize[1]} PanelButton", OversizeButtons.CreatePanelButtonOfSize(xysize[0], xysize[1]));
+        }
+        catch (Exception ex) // Catches all exceptions
+        {
+            if (ex is ArgumentException)
+            {
+                IGConsole.Log($"Error! Cant make button of size {size}, It already exists!");
+            }
+            else
+            {
+                IGConsole.Log($"Error! Cant make button of size {size}, correct format is \"{{X size of button}} {{Y size of button}}\" ");
+            }
+            return false;
+        }
+        string[] file = { };
+        if (File.Exists($"{Directory.GetCurrentDirectory()}/sizes_b.txt"))
+        {
+            file = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/sizes_b.txt");
+        }
+        List<string> list = new List<string>();
+        list.AddRange(file);
+        list.AddRange(new string[]{size});
+        file = list.ToArray();
+        File.WriteAllLines($"{Directory.GetCurrentDirectory()}/sizes_b.txt", file);
+        IGConsole.Log($"Added button of size {size} to component list");
+        return true;
+    }
+}
+
+public class Remove_Button : Command
+{
+    public override string Name => "removeButton";
+    public override string Usage => $"{Name} x_size y_size";
+    public override string Description => "removes a button of specified size to the component list";
+
+    public override bool Execute(IEnumerable<string> args)
+    {
+        if (args.Count() < 2)
+        {
+            IGConsole.Log("Not enough arguments!");
+            return false;
+        }
+        string size = string.Join(" ", args.ToArray()); //turns the array into a string
+        try
+        {
+            int[] xysize = Array.ConvertAll(size.Split(' '), s => int.Parse(s)); // split the string into a string array at every space and convert that into an int array
+        }
+        catch (Exception ex) // Catches all exceptions
+        {
+            IGConsole.Log($"Error! Cant remove button of size {size}, correct format is \"{{X size of button}} {{Y size of button}}\" ");
+            return false;
+        }
+        string[] file = { };
+        if (File.Exists($"{Directory.GetCurrentDirectory()}/sizes_b.txt"))
+        {
+            file = File.ReadAllLines($"{Directory.GetCurrentDirectory()}/sizes_b.txt");
+			if (file.Contains(size)) 
+            {
+                List<string> fileList = file.ToList();
+                fileList.Remove(size);
+                file = fileList.ToArray();
+			}
+			else 
+            {
+                IGConsole.Log($"Error! Cant remove button of size {size}, It doesn't exist!");
+                return false;
+            }
+            File.WriteAllLines($"{Directory.GetCurrentDirectory()}/sizes_b.txt", file);
+        }
+		else 
+        {
+            IGConsole.Log($"Error! Cant remove button of size {size}, There are no custom buttons to delete!");
+            return false;
+        }
+        IGConsole.Log($"Reomved button of size {size} from component list. NOTE: this change will only take effect when you restart your game!!!");
+        return true;
     }
 }
