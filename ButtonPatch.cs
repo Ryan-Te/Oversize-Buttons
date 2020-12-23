d2b1b630f336428250577422c06a7a858351803f
Ryan T <46229874+Ryan-Te@users.noreply.github.com>
2020-12-22 23:26:51 -0500

    Update ButtonPatch.cs

--- ButtonPatch.cs
+++ ButtonPatch.cs
@@ -1,8 +1,8 @@
-﻿// problem: in each Button's Awake() function, it assigns its own default position to return the visual button to after it is let up. However, in some of the
+// problem: in each Button's Awake() function, it assigns its own default position to return the visual button to after it is let up. However, in some of the
 // components this mod adds, the default position must be changed.
 // Solution: patch the ButtonDown and ButtonUp methods, and handle button position changes in a new way.
 
-// This is an incredibly awkward shitty hacky way of doing this. The reason for this is twofold: firstly, I don't know how to use this framework properly, and secondly,
+// This is an incredibly awkward bad hacky way of doing this. The reason for this is twofold: firstly, I don't know how to use this framework properly, and secondly,
 // I didn't write the game's code in a way that anticipated modding. The second issue is one I intend to fix in future versions of TUNG.
 
 
@@ -15,7 +15,7 @@ public class ButtonPatch
     private static GameObject VisualButtonBeingPressed;
     private static Vector3 OriginalLocalPositionOfVisualButtonBeingPressed;
 
-    [PatchMethod(OriginalMethod = "ButtonDown", PatchType = PatchType.Prefix)]
+    [PatchMethod("ButtonDown", PatchType.Prefix)]
     static void ButtonDownPre()
     {
         RaycastHit hit;
@@ -24,13 +24,13 @@ public class ButtonPatch
         OriginalLocalPositionOfVisualButtonBeingPressed = VisualButtonBeingPressed.transform.localPosition;
     }
 
-    [PatchMethod(OriginalMethod = "ButtonDown", PatchType = PatchType.Postfix)]
+    [PatchMethod("ButtonDown", PatchType.Postfix)]
     static void ButtonDownPost()
     {
         VisualButtonBeingPressed.transform.localPosition = OriginalLocalPositionOfVisualButtonBeingPressed - new Vector3(0, 0.15f, 0);
     }
 
-    [PatchMethod(PatchType = PatchType.Postfix)]
+    [PatchMethod(PatchType.Postfix)]
     static void ButtonUp()
     {
         VisualButtonBeingPressed.transform.localPosition = OriginalLocalPositionOfVisualButtonBeingPressed;
